f6fc7214531b324fe5f1eaf374c5e7ce6b13b152
Márk Polák <polak@cngroup.dk>
2021-12-23 19:18:33 +0100

    Added delete user command

---
+++ SeeTrue.Infrastructure/Commands/DeleteUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SeeTrue.Infrastructure.Commands
+{
+    public static class DeleteUser
+    {
+        public record Command(Guid UserId) : IRequest;
+
+        public class Handler : IRequestHandler<Command>
+        {
+            public Handler()
+            {
+
+            }
+
+            public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
