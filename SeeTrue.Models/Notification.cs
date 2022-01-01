322b1a65ef2da7238f63b22cc38848b0a22fc024
Márk Polák <polak@cngroup.dk>
2022-01-01 15:19:27 +0100

    Added basic email template

---
+++ SeeTrue.Models/Notification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeeTrue.Models
+{
+    public enum NotificationType
+    {
+        Confirmation,
+        EmailChange,
+        InviteUser,
+        MagicLink,
+        Recovery
+    }
+
+    public class Notification
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public NotificationType Type { get; set; }
+        public string Language { get; set; }
+        public string Template { get; set; }
+        public string Content { get; set; }
+        public string Audience { get; set; }
+    }
+}
