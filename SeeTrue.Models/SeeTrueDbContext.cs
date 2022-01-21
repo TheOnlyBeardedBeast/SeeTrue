394043b708e89f1f006b9eca4ab5758e4365a74f
Márk Polák <polak@cngroup.dk>
2022-01-21 22:54:58 +0100

    Added instance specific mails

--- SeeTrue.Models/SeeTrueDbContext.cs
+++ SeeTrue.Models/SeeTrueDbContext.cs
@@ -23,7 +23,7 @@ namespace SeeTrue.Models
             modelBuilder.Entity<User>().Property<string>("AppMetaDataJson");
 
             modelBuilder.Entity<User>().HasIndex(e => new { e.Email, e.InstanceID, e.Aud }).IsUnique();
-            modelBuilder.Entity<Mail>().HasIndex(e => new { e.Language, e.Type, e.Audience }).IsUnique();
+            modelBuilder.Entity<Mail>().HasIndex(e => new { e.InstanceId, e.Language, e.Type, e.Audience }).IsUnique();
 
             base.OnModelCreating(modelBuilder);
         }
